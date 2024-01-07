b58f9663660c565518f15645964e3b0731c28f5f
tuananhhdev <tuananh@gmail.com>
2024-01-08 03:19:02 +0700

    update layout trang admin (quan ly phim)

--- src/utils/Local.js
+++ src/utils/Local.js
@@ -1,5 +1,7 @@
 export const setLocal = (data, key = "user_token") => {
-  localStorage.setItem(key, JSON.stringify(data));
+  // localStorage.setItem(key, JSON.stringify(data));
+  const dataJson = JSON.stringify(data);
+  localStorage.setItem(key, dataJson);
 };
 
 export const getLocal = (key = "user_token") => {
