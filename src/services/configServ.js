4b6a53bcc7d6ae14df0f6b49172a96c046290026
Nhat Sang Nguyen <93591100+nnhatsang@users.noreply.github.com>
2024-05-31 13:39:10 +0700

    Update

--- src/services/configServ.js
+++ src/services/configServ.js
@@ -6,7 +6,7 @@ export const API = axios.create({
   // đoạn đầu URL sử dụng chung của tất cả các request
   baseURL: "https://movienew.cybersoft.edu.vn/",
   headers: {
-    TokenCybersoft: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAwOSIsIkhldEhhblN0cmluZyI6IjE4LzA1LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxNTk5MDQwMDAwMCIsIm5iZiI6MTY5MjI5MTYwMCwiZXhwIjoxNzE2MTM4MDAwfQ.qCglC_oyHM79HVc5mRXJfocVkww4VUpWO7ug7MWtJoY`,
+    TokenCybersoft: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAwMSIsIkhldEhhblN0cmluZyI6IjMwLzA5LzIwMzEiLCJIZXRIYW5UaW1lIjoiMTk0ODQ5MjgwMDAwMCIsIm5iZiI6MTYwMTIyNjAwMCwiZXhwIjoxOTQ4NjQwNDAwfQ.4l-eTzlgVnFczfvc2Or7BNPOcaesY3Kwc8RoNm-o-6M`,
     Authorization: `Bearer ${dataUser ? dataUser.accessToken : null}`,
   },
   timeout: 15000,
